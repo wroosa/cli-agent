081e0c0255a2d07ec85e4d9854339fa1f7d59a59
Walter R <wroosa111@gmail.com>
2025-09-03 16:06:39 -0400

    Add arg parsing and one shot response
    Add arg parsing with error messaging and send a one time prompt to googles free LLM and display the response. Also added a small calculator app for testing the tools functionality for working on project directories.

---
+++ main.py
@@ -0,0 +1,46 @@
+import os, sys, argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+
+    # argument parsing and error logic
+    class MyParser(argparse.ArgumentParser):
+        def error(self, message):
+            sys.stderr.write(f"Error: {message}\n\n")
+            self.print_help()
+            sys.exit(1)   # change to exit code 1
+
+    parser = MyParser(description="CLI coding agent")
+    parser.add_argument("prompt", help="the prompt you wish to send to the agent")
+    parser.add_argument("--verbose", "-v", action="store_true")
+
+    args = parser.parse_args()
+
+    limiter = '(Limit your response to one paragraph)'
+
+    # Store messages
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=(args.prompt + limiter))])
+    ]
+
+    if args.verbose: print(f'User prompt: {args.prompt}')
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+        )
+    
+    print(response.text)
+    if args.verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    
+
+
+if __name__ == "__main__":
+    main()
